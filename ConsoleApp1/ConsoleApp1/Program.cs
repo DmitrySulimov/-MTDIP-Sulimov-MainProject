578b375e4acd530d300ef16845b7427c8f76fbab
Dmitry Sulimov <sulimovdmitryvnua@gmail.com>
2018-11-05 22:56:15 +0200

    desktop second changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,10 +8,16 @@ namespace ConsoleApp1
 {
     class Program
     {
-        private static int a, b, sum;
+        private static int a, b, diff;
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите число а");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            b = Int32.Parse(Console.ReadLine());
+            diff = a - b;
+            Console.WriteLine("Ваша сумма равна {0}", diff);
+            Console.ReadKey();
         }
     }
 }
